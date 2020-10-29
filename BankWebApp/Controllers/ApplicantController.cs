01b727eabc80d336401768f32e681f4a8fe23023
Iftikhar Siddique <robustsystems@hotmail.co.uk>
2020-10-29 19:41:12 +0000

    Added BankWebApp project.

---
+++ BankWebApp/Controllers/ApplicantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankWebApp.Models;
+
+namespace BankWebApp.Controllers
+{
+    public class ApplicantController : Controller
+    {
+
+        public ApplicantController()
+        {
+
+        }
+
+        // GET: Applicant/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromBody] ApplicantModel applicantModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(applicantModel);
+        }
+    }
+}
---
+++ BankWebApp/Models/ApplicantModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankWebApp.Models
+{
+  public class ApplicantModel
+  {
+    public ApplicantModel()
+    {
+
+    }
+
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+
+    public int AnnualIncome { get; set; }
+
+  }
+}
