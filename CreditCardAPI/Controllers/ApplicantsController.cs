b5000c203d3675e5df638f24392996e4ee65ec4f
Iftikhar Siddique <robustsystems@hotmail.co.uk>
2020-10-29 13:56:47 +0000

    Added Web API project.

---
+++ CreditCardAPI/Controllers/ApplicantsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditCardAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ApplicantsController : ControllerBase
+  {
+    // GET: api/<ApplicantsController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "Applicant1", "Applicant2" };
+    }
+
+    // POST api/<ApplicantsController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+  }
+}
---
+++ CreditCardAPI/Controllers/CreditCardsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CreditCardAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CreditCardsController : ControllerBase
+  {
+    // GET: api/<CreditCardController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<CreditCardController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/<CreditCardController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+  }
+}
