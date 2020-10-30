ae3b83d42b6c13ad765c181a285b7fba882e1d8a
Iftikhar Siddique <robustsystems@hotmail.co.uk>
2020-10-30 08:02:43 +0000

    Added EligibleCard view.

--- BankWebApp/Controllers/ApplicantController.cs
+++ BankWebApp/Controllers/ApplicantController.cs
@@ -5,38 +5,48 @@ using System;
 namespace BankWebApp.Controllers
 {
   public class ApplicantController : Controller
+  {
+
+    public ApplicantController()
     {
 
-        public ApplicantController()
-        {
-
-        }
-
-        // GET: Applicant/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Applicant/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(ApplicantModel applicantModel)
-        {
-            if (ModelState.IsValid)
-            {
-                // _service.ProcessApplicant(firstName... DoB);
-                //return RedirectToAction(nameof(Index));
-                return View("Details", applicantModel);
-            }
-
-            return View(applicantModel);
-        }
-
-        // GET: Applicant/Create
-        public IActionResult Details(ApplicantModel applicantModel)
-        {
-            return View();
-        }
     }
+
+    // GET: Applicant/Create
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: Applicant/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(ApplicantModel applicantModel)
+    {
+      if (ModelState.IsValid)
+      {
+        // _service.ProcessApplicant(firstName... DoB);
+        //return RedirectToAction(nameof(Index));
+
+        //return View("Details", applicantModel);
+
+        var cardModel = new CardModel
+        {
+          CardName = "Barclaycard",
+          APR = 19.5M,
+          PromotionalMessage = "Welcome to Barclaycard!"
+        };
+
+        return View("EligibleCard", cardModel);
+      }
+
+      return View(applicantModel);
+    }
+
+    // GET: Applicant/Create
+    public IActionResult Details(ApplicantModel applicantModel)
+    {
+      return View();
+    }
+  }
 }
