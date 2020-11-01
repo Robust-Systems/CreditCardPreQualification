661f45511c3a4a68605e3e12ac52d975d9f212ed
Iftikhar Siddique <robustsystems@hotmail.co.uk>
2020-11-01 23:12:40 +0000

    Removed redundant CreditCardAPI project and redundant Views in the web app project.

--- BankWebApp/Controllers/CardController.cs
+++ BankWebApp/Controllers/CardController.cs
@@ -14,12 +14,6 @@ namespace BankWebApp.Controllers
       _dataLogicService = dataLogicService;
     }
 
-    // GET: CardController
-    public ActionResult Index()
-    {
-      return View();
-    }
-
     public ActionResult ApplicantResult(int applicationLogID)
     {
       var applicant = _dataLogicService.GetApplicant(applicationLogID);
@@ -39,82 +33,6 @@ namespace BankWebApp.Controllers
       }
 
       return View(cardModel);
-    }
-
-    // GET: CardController/Details/5
-    public ActionResult Details(int id)
-    {
-      var model = new CardModel
-      {
-        CardName = "Barclaycard",
-        APR = 19.5M,
-        PromotionalMessage = "Welcome to Barclaycard!"
-      };
-
-      return View(model);
-    }
-
-    // GET: CardController/Create
-    public ActionResult Create()
-    {
-      return View();
-    }
-
-    // POST: CardController/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-
-    // GET: CardController/Edit/5
-    public ActionResult Edit(int id)
-    {
-      return View();
-    }
-
-    // POST: CardController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-
-    // GET: CardController/Delete/5
-    public ActionResult Delete(int id)
-    {
-      return View();
-    }
-
-    // POST: CardController/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
     }
   }
 }
